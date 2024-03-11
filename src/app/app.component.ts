9c9ebaa45917dcc460e5174a460837bac5cf2417
Andre Fabiano A Lima <andregtalima@gmail.com>
2024-03-10 23:37:52 -0300

    commit initial project portifolio-web

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, HeaderComponent, MainComponent],
 })
 export class AppComponent {
   title = 'portifolio-web';
